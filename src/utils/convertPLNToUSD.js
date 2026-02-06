8766207e65bb2db0c3343594e7f0d37655ed1611
Wojciech Johaniuk <wojciech.johaniuk@gmail.com>
2026-02-06 13:21:25 +0100

    add unit tests for ConvertPLNtoUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,20 @@
 export const convertPLNToUSD = (PLN) => {
+	if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+		return 'Error';
+	}
+	if (typeof PLN === 'string' || isNaN(PLN)) {
+		return NaN;
+	}
+	if (PLN < 0) {
+		return '$0.00';
+	}
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+	const PLNtoUSD = PLN / 3.5;
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
+
+	return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+};
